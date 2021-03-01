218a223fc505ca44fb6c3a4969736b4d6e1ddf9f
Hgaffa <hgaffa@gmail.com>
2021-03-01 13:28:36 +0000

    Added ADF Test + Data Transform

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from tkinter import filedialog, messagebox, ttk
 
 from eda import EDA
 from ta import TechnicalAnalysis
+from stationary import Stationary
+
+import numpy as np
 
 import pandas as pd
 
@@ -28,24 +31,149 @@ class MainApplication(tk.Frame):
 
         self.generate_ta()
     
-        self.close_plot_frame = tk.LabelFrame(root, text="Close Price")
-        self.close_plot_frame.place(height=350, width=500, rely=0.3, relx=0)
+        #initialise frame for changing plot data (dropdown menu)
+        self.choose_data = tk.LabelFrame(root, text="Choose Plot Data")
+        self.choose_data.place(height=50, width=200, rely=0.3, relx=0)
+
+        #initialise frame for plotting data
+        self.close_plot_frame = tk.LabelFrame(root, text="Data Plot")
+        self.close_plot_frame.place(height=400, width=1000, rely=0.35, relx=0)
+
+        self.initialise_feature_dropdown()
 
         self.plotter = None
 
-        self.prices = None
+    #function to initialise dropdown menu in data plot frame and plot button
+    def initialise_feature_dropdown(self):
+
+        self.variable = tk.StringVar(root)
+        self.variable.set("Close") #default value
+
+        self.w = OptionMenu(self.choose_data, self.variable, *['Close'])
+        self.w.place(relx=0.05, rely=0.1)
+
+        plot_button = tk.Button(self.choose_data, text="Plot Feature", command=lambda: self.update_plot())
+        plot_button.place(relx=0.55, rely=0.16)
+
+    def update_plot(self):
+
+        if self.plotter:
+
+            self.plotter.destroy()
+        #Plot chosen feature
+        plt.rc('xtick',labelsize=6)
+        plt.rc('ytick',labelsize=6)
+
+        fig = Figure(figsize=(5,10))
+
+        ax = fig.add_subplot(111)
+
+        ax.plot(self.df[self.variable.get()])
+
+        ax.set_title(self.variable.get(), fontsize=6)
+        ax.set_ylabel(self.variable.get(), fontsize=6)
+        ax.set_xlabel("Date", fontsize=6)
+
+        canvas = FigureCanvasTkAgg(fig,master=self.close_plot_frame)
+        self.plotter = canvas.get_tk_widget()
+        self.plotter.pack(fill='both')
+        canvas.draw()
+
+        ax.clear()
+
+    def feature_dropdown_update(self):
+
+        #update dropdown menu items
+        self.w['menu'].delete(0, 'end')
+        self.variable.set("Close")
+        for col in self.df.columns:
+            self.w['menu'].add_command(label=col, command=tk._setit(self.variable, col))
 
     def generate_ta(self):
 
         self.ta_frame = tk.LabelFrame(root, text="Feature Engineering")
-        self.ta_frame.place(height=50, width=200, rely=0.65, relx=0.055)
+        self.ta_frame.place(height=50, width=200, rely=0.2, relx=0.22)
 
         button2 = tk.Button(self.ta_frame, text="Generate TA Features", command=lambda: self.update_tree_ta())
         button2.place(relx=0.5, rely=0.5, anchor='center')
 
+    #function to initialize frame and button for detrending time series
+    def stationarize(self):
+
+        #frame for adf test + output
+        self.stationary_frame = tk.LabelFrame(root, text="ADF Test/Stationarity Output")
+        self.stationary_frame.place(height=200, width=1000, rely=0.75, relx=0)
+
+        #frame for transform button for stationarity
+        self.transform_frame = tk.LabelFrame(root, text="Transform Data")
+        self.transform_frame.place(height=50, width=200, rely=0.2, relx=0.34)
+
+        button2 = tk.Button(self.transform_frame, text="Transfrom Data", command=lambda: self.data_transform(),padx=10)
+        button2.place(relx = 0.5, rely= 0.5, anchor='center')
+
+        self.adf_text = StringVar()
+
+        #initialise label to initial close series ADF test stats
+        initial_adf = Stationary(self.df.dropna())
+        
+        check_adf = initial_adf.adf_test(self.df.Close)
+
+        initial_adf_text = "The ADF Statistic of Close Price is: {:.2f} \n\n The p-Value is: {:.2f} \n\n Critical Values: \n ----------- \n".format(initial_adf.adf_stat, initial_adf.p_val)
+
+        for key, val in initial_adf.crit_vals.items():
+            
+            temp = f'{key} : {val}'
+            initial_adf_text += temp + "\n"
+
+        initial_adf_text += "\n The Data is Now Stationary!" if initial_adf.p_val < initial_adf.THRESH else "\n The Data is Not Stationary!"
+
+        self.adf_text.set(initial_adf_text)
+        adf_label = tk.Label(self.stationary_frame, textvariable=self.adf_text).place(relx=0.5, rely=0.5, anchor='center')
+
+
+    #function to transform data and make stationary
+    def data_transform(self):
+
+        data_st = pd.DataFrame(self.df.fillna(method='bfill')).applymap(lambda x: np.NAN if x in [np.inf, -np.inf] else x).dropna()
+
+        stationary = Stationary(data_st)
+
+        print(self.df)
+
+        self.og_df = self.df
+
+        #update df to be stationary df
+        self.df = stationary.st()
+
+        check_adf = stationary.adf_test(self.df.Close)
+
+        adf_label_text = "The ADF Statistic of Close Price is: {:.2f} \n\n The p-Value is: {:.2f} \n\n Critical Values: \n ----------- \n".format(stationary.adf_stat, stationary.p_val)
+
+        for key, val in stationary.crit_vals.items():
+            
+            temp = f'{key} : {val}'
+            adf_label_text += temp + "\n"
+
+        adf_label_text += "\n The Data is Now Stationary!" if stationary.p_val < stationary.THRESH else "\n The Data is Not Stationary!"
+
+
+        self.adf_text.set(adf_label_text)
+
+        #update tree view
+        self.tree_update()
+
+        return None
+
     def update_tree_ta(self):
 
         self.df = TechnicalAnalysis(self.df).ta()
+
+        self.tree_update()
+
+        self.feature_dropdown_update()
+
+    #function to refresh values in price data tree view
+    def tree_update(self):
 
         self.clear_data()
 
@@ -61,9 +189,9 @@ class MainApplication(tk.Frame):
     def eda(self):
         #Frame for performing EDA
         self.eda_frame = tk.LabelFrame(root, text="EDA")
-        self.eda_frame.place(height=50, width=100, rely=0.65, relx=0)
+        self.eda_frame.place(height=50, width=185,  rely=0.3, relx=0.11)
 
-        button3 = tk.Button(self.eda_frame, text="Perform EDA", command=lambda: EDA(root, self.prices))
+        button3 = tk.Button(self.eda_frame, text="Perform EDA", command=lambda: EDA(root, self.df[self.variable.get()]))
         button3.place(relx=0.5, rely=0.5, anchor='center')
 
     def initialise_open_file(self):
@@ -81,7 +209,6 @@ class MainApplication(tk.Frame):
         # The file/file path text
         self.label_file = ttk.Label(self.file_frame, text="No File Selected")
         self.label_file.place(rely=0, relx=0)
-
 
     def create_window(self):
         window = tk.Toplevel(root)
@@ -130,11 +257,6 @@ class MainApplication(tk.Frame):
         return data
 
     def Load_data(self):
-        
-        if self.plotter:
-
-            self.plotter.destroy()
-
         file_path = self.label_file["text"]
 
         filename = r"{}".format(file_path)
@@ -152,31 +274,10 @@ class MainApplication(tk.Frame):
         for row in df_rows:
             self.tv1.insert("", "end", values=row)
 
-        self.prices = self.df.Close
+        #set inital adf values
+        self.stationarize()
 
-        self.plot_close_price(prices=self.prices)
-
-    def plot_close_price(self, prices):
-        #Plot close prices
-        plt.rc('xtick',labelsize=6)
-        plt.rc('ytick',labelsize=6)
-
-        fig = Figure(figsize=(5,10))
-
-        ax = fig.add_subplot(111)
-
-        ax.plot(prices)
-
-        ax.set_title("Close Price", fontsize=6)
-        ax.set_ylabel("Price", fontsize=6)
-        ax.set_xlabel("Date", fontsize=6)
-
-        canvas = FigureCanvasTkAgg(fig,master=self.close_plot_frame)
-        self.plotter = canvas.get_tk_widget()
-        self.plotter.pack(fill='both')
-        canvas.draw()
-
-        ax.clear()
+        self.feature_dropdown_update()
 
     def clear_data(self):
         self.tv1.delete(*self.tv1.get_children())
@@ -186,5 +287,5 @@ if __name__ == "__main__":
     root.title("Financial ML Project Demo")
     root.state("zoomed")
     MainApplication(root).pack(side="top", fill="both", expand=True)
-    root.grid_propagate(False)
+    root.pack_propagate(False)
     root.mainloop()
