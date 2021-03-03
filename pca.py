c3dfecb3a404b60f4442060d3af725da00552880
Hgaffa <hgaffa@gmail.com>
2021-03-03 11:30:48 +0000

    3D PCA Plot

--- pca.py
+++ pca.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
+from mpl_toolkits.mplot3d import Axes3D
 
 import pandas as pd
 
@@ -30,7 +31,7 @@ class PCAPlot(Toplevel):
         self.plotter=None
 
         #Frame for pca plot
-        self.pca_frame = tk.LabelFrame(self, text="Correlation with future returns")
+        self.pca_frame = tk.LabelFrame(self, text="PCA Plot of Top 3 Principal Components")
         self.pca_frame.place(height=700, width=800, rely=0, relx=0)
 
         self.df = data
@@ -52,19 +53,46 @@ class PCAPlot(Toplevel):
 
         X_copy_scaled = scaler.fit_transform(X_copy)
 
-        pca = PCA(n_components=2)
+        pca = PCA(n_components=3)
 
         pca.fit(X_copy_scaled)
 
-        X_pca = pca.transform(X_copy_scaled)
+        X_pca=pd.DataFrame(pca.transform(X_copy_scaled), columns=['PCA%i' % i for i in range(3)])
 
         print("Old Shape: ", self.df.shape)
 
         print("New Shape: ", X_pca.shape)
 
         fig, ax = plt.subplots(figsize=(15,10))
+        ax = fig.add_subplot(111, projection='3d')
 
-        ax.scatter(X_pca[:,0], X_pca[:,1],c=self.df['Labels'])
+        plot_colours = []
+
+        for i in self.df['Labels'].astype(int):
+
+            if i == 1:
+
+                plot_colours.append('blue')
+            
+            else:
+
+                plot_colours.append('yellow')
+
+        ax.scatter(X_pca['PCA0'], X_pca['PCA1'], X_pca['PCA2'], c=plot_colours, cmap="Set2_r", s=60)
+        
+        # make simple, bare axis lines through space:
+        xAxisLine = ((min(X_pca['PCA0']), max(X_pca['PCA0'])), (0, 0), (0,0))
+        ax.plot(xAxisLine[0], xAxisLine[1], xAxisLine[2], 'r')
+        yAxisLine = ((0, 0), (min(X_pca['PCA1']), max(X_pca['PCA1'])), (0,0))
+        ax.plot(yAxisLine[0], yAxisLine[1], yAxisLine[2], 'r')
+        zAxisLine = ((0, 0), (0,0), (min(X_pca['PCA2']), max(X_pca['PCA2'])))
+        ax.plot(zAxisLine[0], zAxisLine[1], zAxisLine[2], 'r')
+        
+        # label the axes
+        ax.set_xlabel("PC1")
+        ax.set_ylabel("PC2")
+        ax.set_zlabel("PC3")
+        ax.set_title("PCA on the iris data set")
 
         canvas = FigureCanvasTkAgg(fig,master=self.pca_frame)
         self.plotter = canvas.get_tk_widget()
